4f6f23337a81793e82ec99a345326875d27a3b27
Uknow <uknowsec@gmail.com>
2018-12-31 13:07:44 +0800

    Add files via upload

---
+++ demo1/checkcode.php
@@ -0,0 +1,37 @@
+<?php
+	header("Content-Type:text/html;charset=utf-8");
+	session_start();
+	//获取用户输入的验证码
+	$code = trim($_POST["captcha"]);
+	//将字符串都转成小写进行比较
+	if(strtolower($code)==strtolower($_SESSION['captcha_code']))
+	{
+		echo "Verification code right!";
+		//获取用户名和密码
+		$username = trim($_POST['username']);
+		$password = trim($_POST['password']);
+		if(($username=='')||($password==''))
+		{
+			header('refresh:3;url=index.html');
+			echo "The user name or password cannot be empty，3s after the jump to the login page!";
+			exit;
+		}
+		else if(($username!='admin')||($password!='admin'))
+		{
+			//用户名或密码错误
+			header('refresh:3;url=index.html');
+			echo "The user name or password error，3s after the jump to the login page!";
+			exit;
+		}
+		else if(($username=='admin')&&($password=='admin'))
+		{
+			//登录成功将信息保存到session中
+			$_SESSION['username']=$username;
+			echo '<br>Hello:'.$username.' Login successful!';
+		}
+	}
+	else
+	{
+		echo "Verification code error!";
+	}
+?>
---
+++ demo3/checkcode.php
@@ -0,0 +1,26 @@
+<?php
+	header("Content-Type:text/html;charset=utf-8");
+	session_start();
+	//获取用户输入的验证码
+		$username = trim($_POST['username']);
+		$password = trim($_POST['password']);
+		if(($username=='')||($password==''))
+		{
+			header('refresh:3;url=index.html');
+			echo "The user name or password cannot be empty，3s after the jump to the login page!";
+			exit;
+		}
+		else if(($username!='admin')||($password!='admin'))
+		{
+			//用户名或密码错误
+			header('refresh:3;url=index.html');
+			echo "The user name or password error，3s after the jump to the login page!";
+			exit;
+		}
+		else if(($username=='admin')&&($password=='admin'))
+		{
+			//登录成功将信息保存到session中
+			$_SESSION['username']=$username;
+			echo '<br>Hello:'.$username.' Login successful!';
+		}
+?>
